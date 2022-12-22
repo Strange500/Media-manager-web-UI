bcb67720d7811188807e985fabed8182253b1829
Strange500 <93034371+Strange500@users.noreply.github.com>
2022-12-22 14:40:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -341,19 +341,18 @@ if ("search.html" === path) {
 }
 
 
-else  {
-    alive();
-    cpu_temp();
-    serv_log();
-    space();
-    stat_lib()
-    setInterval(alive, 5000)
-    setInterval(cpu_temp, 5000)
-    setInterval(serv_log, 10000)
+alive();
+cpu_temp();
+serv_log();
+space();
+stat_lib()
+setInterval(alive, 5000)
+setInterval(cpu_temp, 5000)
+setInterval(serv_log, 10000)
 
 
 
-}
+
 
 
 button_container.addEventListener("click", function () {
